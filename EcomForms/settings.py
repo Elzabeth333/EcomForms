7225c32b100cdc1c3966c2a927998faa84ddf09e
Elzabeth333 <elzabethjoseph333@gmail.com>
2024-09-27 10:57:07 +0530

    first commit

--- EcomForms/settings.py
+++ EcomForms/settings.py
@@ -125,7 +125,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -137,3 +146,4 @@ MEDIA_URL = '/media/'
 
 # Directory where media files are stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
